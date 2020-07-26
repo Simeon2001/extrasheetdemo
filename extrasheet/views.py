85eaa3fe56d228595c634527101da385cff3128f
Simeon2001 <jesusanyasimeon@gmail.com>
2020-07-26 01:49:39 +0100

    extrasheet demo

--- extrasheet/views.py
+++ extrasheet/views.py
@@ -11,6 +11,8 @@ from django.shortcuts import redirect
 from django.contrib.auth import views as auth_views
 from extrasheet.utils import create_notify
 from .forms import ForumForm,InsightForm
+from django.contrib.auth.decorators import login_required 
+from django.contrib.auth.mixins import LoginRequiredMixin,UserPassesTestMixin
 # Create your views here.
 def index (request):
     user = request.user
@@ -32,6 +34,7 @@ class RegisterView(generic.CreateView):
     success_url = reverse_lazy('login')
     
 #Profile
+ 
 class ProfilesCreateView(CreateView):
     
     model = Profile
@@ -39,11 +42,13 @@ class ProfilesCreateView(CreateView):
     template_name = 'profile.html'
     
     fields = ['profile_image','school_name','gender','major_courses','minor_courses','personal_interest','skills_you_have','skills_you_like_to_have','hobbies','spoken_languages','heros_or_rolemodels']
+    login_url = 'login' 
     def form_valid(self, form): # new
         form.instance.name = self.request.user
         return super().form_valid(form)
         
 #club form
+
 class ClubCreateView(CreateView):
     model = Club
     success_url = reverse_lazy('Home/')
@@ -54,10 +59,12 @@ class ClubCreateView(CreateView):
         'niche',
         'about_club'
         ]
+    login_url = 'login' 
     def form_valid(self, form): # new
         form.instance.name = self.request.user
         return super().form_valid(form)
 #filter club based on personal interest
+@login_required(login_url='login')    
 def club_view (request):
     current_user = request.user
     a = current_user.id
@@ -72,6 +79,7 @@ def club_view (request):
     return render (request,'join_club.html',{'c':c})
 
 #join 
+@login_required(login_url='login')    
 def join_club (request):
     club = get_object_or_404(Club,id=request.POST.get('club_id'))
     club.join.add(request.user)
@@ -79,6 +87,7 @@ def join_club (request):
     return redirect ('clubl')
 
 #club joined 
+@login_required(login_url='login')    
 def club_join (request):
     cj = request.user.join.all()
     for club in cj:
@@ -87,6 +96,7 @@ def club_join (request):
     b=a
     #create_notify('you are in',cj )
     return render (request,'club_joined.html',{'cj':cj})
+@login_required(login_url='login')    
 def club_forum (request,pi):
     club  = get_object_or_404(Club,pk=pi)
     comm = club.club_comment.all()
@@ -101,11 +111,11 @@ def club_forum (request,pi):
     else:
 	    form = ForumForm()
     return render(request,'forum.html',{'comm':comm,'form':form,'club':club})
-
+@login_required(login_url='login')    
 def clubs (request,pi):
     club  = get_object_or_404(Club,club_name=pi)
     return render (request,'cj.html',{'club':club})
-    
+@login_required(login_url='login')       
 def club_insight (request,pi) :
     current_user = request.user
     a = current_user.id
